837924482d4afcb0ebd26f198ac0fce5276af9cc
Yuwu Xiong <5070319@qq.com>
2025-05-19 20:51:43 +0800

    add WeChat for Linux

---
+++ src/mods/48-weixin-mod/install.sh
@@ -0,0 +1,12 @@
+set -e                  # exit on error
+set -o pipefail         # exit on pipeline error
+set -u                  # treat unset variable as error
+
+print_ok "Install Weixin For Linux v4.0.1"
+
+cat ./WeChatLinux_x86_64.deb.* > /tmp/WeChatLinux_x86_64.deb
+apt install /tmp/WeChatLinux_x86_64.deb
+rm /tmp/WeChatLinux_x86_64.deb
+
+
+judge "Install Weixin For Linux v4.0.1"
